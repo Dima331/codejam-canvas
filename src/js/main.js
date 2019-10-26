a18d0305864048e1dd509a84e547701328d365ac
Barkov Dzmitriy <dimbar331@gmail.com>
2019-10-26 15:20:51 +0300

    feat: add 32x32 canvas

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,22 @@
 function draw() {
     var ctx = document.getElementById('canvas').getContext('2d');
-    for (var i = 0; i < data.length; i++) {
-        for (var j = 0; j < data.length; j++) {
-            console.log(data[i][j])
-            ctx.fillStyle = "#" + data[i][j];
-            ctx.fillRect(j * 128, i * 128, 128, 128);
+    // 1 матрица
+    // for (var i = 0; i < data.length; i++) {
+    //     for (var j = 0; j < data.length; j++) {
+    //         console.log(data[i][j])
+    //         ctx.fillStyle = "#" + data[i][j];
+    //         ctx.fillRect(j * 128, i * 128, 128, 128);
+    //     }
+    // }
+    // 2 матрица
+    for (var i = 0; i < dataRgba.length; i++) {
+        for (var j = 0; j < dataRgba.length; j++) {
+            ctx.fillStyle = 'rgba(' +
+                dataRgba[i][j][0] + ',' +
+                dataRgba[i][j][1] + ',' +
+                dataRgba[i][j][2] + ',' +
+                dataRgba[i][j][3] + ')';
+            ctx.fillRect(j * 16, i * 16, 16, 16);
         }
     }
 }
